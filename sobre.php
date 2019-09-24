79732d55e5d533eeefc4d1b76c87f23479d75f5c
Dalton Felix <daltonfelixn@gmail.com>
2019-09-24 16:23:01 -0300

    commit3

--- sobre.php
+++ sobre.php
@@ -3,45 +3,73 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-            <!--Import Google Icon Font-->
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-            <link href="https://fonts.googleapis.com/css?family=Ranga&display=swap" rel="stylesheet">
-            <!--Import materialize.css-->
-            <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-            <link rel="stylesheet" type="text/css" href="css/style.css">
-            <!-- FontAwesome Icons -->
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-            <!--Let browser know website is optimized for mobile-->
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta charset="utf-8">
-            <title>Sapatop</title>
-        </head>
     <body>
-        <!-- Menu tela grande -->
-            <header>
-                <nav class="purple darken-4">
-                    <div class="nav-wrapper">
-                        <a href="index.php" class="brand-logo" style="font-family: 'Ranga', cursive; left: 150px; font-size:50px;">Sapatop</a>
-                        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                        <ul class="right hide-on-med-and-down">
-                            <?php if(isset($_SESSION['id']) && !empty($_SESSION['id'])) { ?>
-                    <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="./cadastro/logout.php">Sair</a></li>
-                  <?php } else { ?>
-                            <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="./cadastro/login.php">Login</a></li>
-                <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="./cadastro/cadastrar.php">Cadastro</a></li>
-              <?php } ?>
-                        </ul>
-                    </div>
-                </nav>
+    <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ranga&display=swap" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!-- FontAwesome Icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="utf-8">
+    <title>Sapatop</title>
+  </head>
+  <body>
+    <!-- Menu tela grande -->
+    <header>
+      <nav style="background-color: #faf2ee">
+        <ul id="dropdown1" class="dropdown-content">
+          <?php if(isset($_SESSION['id']) && !empty($_SESSION['id'])) { ?>
+          <li><a href="./cadastro/logout.php">Sair</a></li>
+          <?php } else { ?>
+          <li><a href="./cadastro/login.php">Login</a></li>
+          <li class="divider"></li>
+          <li><a href="./cadastro/cadastrar.php">Cadastro</a></li>
+          <?php } ?>
+        </ul>
+        <ul id="dropdown2" class="dropdown-content">
+          <li><a href="#!">one</a></li>
+          <li><a href="#!">two</a></li>
+          <li class="divider"></li>
+          <li><a href="#!">three</a></li>
+        </ul>
+        <ul id="dropdown3" class="dropdown-content">
+          <li><a href="#!">one</a></li>
+          <li><a href="#!">two</a></li>
+          <li class="divider"></li>
+          <li><a href="#!">three</a></li>
+        </ul>
+        <div class="nav-wrapper">
+          <a href="index.php" class="brand-logo center" style="font-family: 'Ranga', cursive; font-size:50px; color: #4b392e;">Sapatop</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-          <!-- Menu mobile -->
-                <ul class="sidenav" id="mobile-demo">
-                    <li><a href="sobre.php">Sobre</a></li>
-                    <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="#">Login</a></li>
-          <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="#">Cadastro</a></li>
-            </header>
-
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1" style="color: #4b392e;"><i class="material-icons right">perm_identity arrow_drop_down</i></a></li>
+          </ul>
+          <ul class="right">
+            <li><a href="#!" style="color: #4b392e;">Promoções<i class="material-icons right"></i></a></li>
+          </ul>
+          <ul class="right hide-on-med-and-down">
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown2" style="color: #4b392e;">Sapatos<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+          <ul class="right hide-on-med-and-down">
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown3" style="color: #4b392e;">Sandalias<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+        </div>
+      </nav>
+      <!-- Menu mobile -->
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="sobre.php">Sobre</a></li>
+        <li><a href="feminino.php">Feminino</a></li>
+        <li><a href="masculino.php">Masculino</a></li>
+        <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="#">Login</a></li>
+        <li><a class="waves-effect waves-light btn #6a1b9a purple darken-3" href="#">Cadastro</a></li>
+        <li><a href="#!" style="color: #4b392e;">Promoções<i class="material-icons right"></i></a></li>
+      </header>
        <section>
             <h3 class="center-align">Quem somos</h3>
             <div class="container">
@@ -72,18 +100,18 @@
                 </div>
             </div>
         </section>
-        <section class="servicos purple darken-4">
+        <section class="servicos"style="background-color: #f5e2d8;" >
             <div class="container">
                 <div class="row">
-                    <h3 class="center-align white-text">Serviços</h3>
-                    <p class="center-align white-text">
+                    <h3 class="center-align" style="color: #674c4c">Serviços</h3>
+                    <p class="center-align"style="color: #674c4c">
                         Um pouco de nosso serviço.
                     </p>
                     <div class="col s12 m6 l3 center center-align">
                         <div class="center promo promo-example">
-                            <i class="large material-icons white-text">flash_on</i>
-                            <p class="promo caption white-text">Rapidez</p>
-                            <p class="marven center white-text">
+                            <i class="large material-icons"style="color: #674c4c">flash_on</i>
+                            <p class="promo caption"style="color: #674c4c">Rapidez</p>
+                            <p class="marven center"style="color: #674c4c">
                                 Nossa especialidade é fazer as entregas desejadas em um menor tempo possivel para a satisfação de nossos clientes que consumirem nosso produto.
                             </p>
                         </div>
@@ -91,27 +119,27 @@
                     </div>
                     <div class="col s12 m6 l3 center center-align">
                         <div class="center promo promo-example">
-                            <i class="large material-icons white-text">group</i>
-                            <p class="promo caption white-text">Equipe</p>
-                            <p class="marven center white-text">
+                            <i class="large material-icons"style="color: #674c4c">group</i>
+                            <p class="promo caption"style="color: #674c4c">Equipe</p>
+                            <p class="marven center"style="color: #674c4c">
                                 Nossa equipe se empenha para que cada vez mais podemos atender a todos os pedidos dos clientes sempre que possivel.
                             </p>
                         </div>
                     </div>
                     <div class="col s12 m6 l3 center center-align">
                         <div class="center promo promo-example">
-                            <i class="large material-icons white-text">cloud</i>
-                            <p class="promo caption white-text">Seus dados</p>
-                            <p class="marven center white-text">
+                            <i class="large material-icons"style="color: #674c4c">cloud</i>
+                            <p class="promo caption"style="color: #674c4c">Seus dados</p>
+                            <p class="marven center"style="color: #674c4c">
                                 Para nossa é de extrema importância que seus dados estejam seguros ao realizar uma compra ou o cadastro em nosso site.
                             </p>
                         </div>
                     </div>
                     <div class="col s12 m6 l3 center center-align">
                         <div class="center promo promo-example">
-                            <i class="large material-icons white-text">settings</i>
-                            <p class="promo caption white-text">Opções</p>
-                            <p class="marven center white-text">
+                            <i class="large material-icons"style="color: #674c4c">settings</i>
+                            <p class="promo caption "style="color: #674c4c">Opções</p>
+                            <p class="marven center "style="color: #674c4c">
                                 Temos várias opções para que os clientes tenham uma escolha ampla para a compra de produtos, pagamentos e entregas.
                             </p>
                         </div>
@@ -146,45 +174,80 @@
             </div>
         </section>
 
-       <!-- Footer -->
-            <footer class="page-footer purple darken-4">
-                <div class="container">
-                    <div class="row">
-                        <div class="col l6 s12">
-                            <span class="titulo">Categorias</span>
-                    <ul class="total_itens">
-                      <li><a class="white-text" href="feminino.html">Feminino</li></a>
-                        <li><a class="white-text" href="Masculino.html">Masculino</li></a>
-                      </ul>
-                        </div>
-
-                        <div class="col s12 m6 l3">
-                            <h5 class="white-text">Redes Sociais</h5>
-                            
-                                <a class="white-text" href="https://www.facebook.com/Sapatop-1853076451470888/"><i class="fab fa-facebook-square"></a></i>
-                          <a class="white-text" href="https://twitter.com/sapatop1"><i class="fab fa-twitter-square"></a></i>
-                          <a class="white-text" href="https://www.instagram.com/sapatopifsp"><i class="fab fa-instagram"></a></i>
-                          <a class="white-text" href="https://www.tumblr.com/blog/sapatopifsp"><i class="fab fa-tumblr-square"></a></i>
-                          <a class="white-text" href="https://br.pinterest.com/sapatop/"><i class="fab fa-pinterest-square"></a></i>
-                          <br></br>
-                          As fotos aqui veiculadas, logotipo e marca são de propriedade do site www.sapatop.com.br. É vetada a sua reprodução, total ou parcial, sem a expressa autorização da administradora do site Sapatop.<br></br>
-
-                           Sapatop calçados S/A - CNPJ: 85.833.642/0001-24 - Av. Bahia, 1739  - Indaía, São Paulo - SP, 11665-071
-                            
-                        </div>
-                    </div>
-                </div>
-                <div class="footer-copyright">
-                    <div class="container">
-                        © 2019 Sapatop - Todos os direitos reservados
-                    </div>
-                </div>
-            </footer>
+         <!-- FOOTER -->
+      <h3 class="center-align" style="">Novidades</h3>
+      <footer class="page-footer" style="background-color: #f5e2d8;">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col l3 s12">
+                <h5 style="color: #674c4c">Atendimento</h5>
+                <p class="text-lighten-4">
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Contato</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Perguntas Frequentes</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Trocas e Cancelamentos</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Política de Privacidade</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Lojas</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Quem somos</a><br>
+                  <a class="text-lighten-3" style="color: #674c4c" href="#!">Pagamento seguro</a></p>
+              </div>
+              <div class="col l3 s12"> 
+                <h5 style="color: #674c4c">Contato</h5>
+                <p style="color: #674c4c">
+                  Telefone: (51) 3564-6646<br>
+                  Whatsapp: (51) 99273-9456<br>
+                  Horário de atendimento:<br>
+                  Segunda à quinta-feira: 7:00 às 11:00 e 13:00 às 17:00.<br>
+                  Sexta:-feira: 7:00 às 11:00 e 13:00 às 16:00.<br>
+                </p>  
+              </div>
+              <div class="col l6 s12">
+                <h5 style="color: #674c4c">Inscreve-se</h5><p>
+                <form action="/pagina-processa-dados-do-form" method="post">
+                  <div>
+                    <a class="text-lighten-3" style="color: #674c4c" href="#!">Inscreva-se para receber mais novidades!</a><p>
+                    <input type="email" name="email" placeholder="Seu e-mail..." style="background-color: white;">
+                    
+                    
+                  </div>
+              </div>
+            </div>  
+            <div class="row">
+              <div class="col l3 s12">
+                
+              </div>
+              <div class="col l3 s12"> 
+              
+              <ul>
+                  <li>
+                    <h5 style="color: #674c4c">Redes Socias</h5>
+                    <a href="#!">
+                      <img src="https://www.constance.com.br/skin/frontend/constance-v2018/default/images/insta.svg">
+                      <img src="https://www.constance.com.br/skin/frontend/constance-v2018/default/images/facebook.svg">
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                      width="30" height="30"
+                      viewBox="0 0 172 172"
+                      style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#674c4c"><path d="M172.215,35.905c-6.35594,2.82188 -13.16875,4.71656 -20.33094,5.57656c7.31,-4.38063 12.92687,-11.31438 15.56062,-19.565c-6.82625,4.04469 -14.41844,6.9875 -22.4675,8.57312c-6.45,-6.88 -15.64125,-11.16656 -25.81344,-11.16656c-19.53812,0 -35.38094,15.82937 -35.38094,35.3675c0,2.76812 0.3225,5.46906 0.92719,8.0625c-29.40125,-1.47813 -55.45656,-15.56063 -72.91187,-36.96656c-3.05031,5.24062 -4.78375,11.31437 -4.78375,17.79125c0,12.26844 6.235,23.09906 15.73531,29.455c-5.805,-0.18813 -11.26062,-1.78719 -16.03094,-4.43438c0,0.14781 0,0.29563 0,0.44344c0,17.14625 12.20125,31.43031 28.36656,34.69562c-2.95625,0.80625 -6.08719,1.23625 -9.31219,1.23625c-2.28438,0 -4.50156,-0.215 -6.665,-0.645c4.515,14.04219 17.57625,24.295 33.04281,24.57719c-12.09375,9.48688 -27.34531,15.13063 -43.92719,15.13063c-2.86219,0 -5.67062,-0.16125 -8.42531,-0.49719c15.64125,10.05125 34.23875,15.89656 54.22031,15.89656c65.06438,0 100.64688,-53.89781 100.64688,-100.63344c0,-1.53187 -0.04031,-3.07719 -0.09406,-4.58219c6.90687,-4.98531 12.9,-11.22031 17.64344,-18.31531z"></path></g></g></svg>
+                    </a>
+                </ul>
+              </div>
+              <div class="col l5 s12">
+              </div>
+              <div class="col l1 s12">
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            COM VOCÊ, ONDE VOCÊ ESTIVER | NÓS ❤ SAPATOS
+            ©2019 - Tecnologistica Distribuidora E Comercio De Calcados E Bolsas Ltda | CNPJ - 30.901.791/0001-91
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+            
         <!-- Jquery -->
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
         <!-- Materialize JS -->
         <script type="text/javascript" src="js/materialize.min.js"></script>
-        </script>
         <script type="text/javascript">
             $(document).ready(function(){
                 $('.sidenav').sidenav();
