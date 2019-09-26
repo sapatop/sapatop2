6c6ddb16e809b3829e7e37460f4914ed1de0e1c9
Dalton Felix <daltonfelixn@gmail.com>
2019-09-26 16:16:31 -0300

    4commit
    tirei o a

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-a
   <head>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
